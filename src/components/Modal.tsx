493d00a3bea8543bee55d7b43e8a24eaa3692a41
mini <0miiii@naver.com>
2023-05-31 18:23:30 +0900

    feat: Modal 컴포넌트 추가

---
+++ src/components/Modal.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+interface IProps {
+  children: React.ReactNode;
+  onClose: () => void;
+}
+
+const Modal: React.FC<IProps> = ({ children, onClose }) => {
+  const portalElement = document.getElementById("overlays") as
+    | Element
+    | DocumentFragment;
+
+  return ReactDOM.createPortal(
+    <>
+      <div
+        className="fixed top-0 left-0 w-full h-screen z-40 bg-black bg-opacity-75"
+        onClick={onClose}
+      />
+      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 mx-auto bg-white rounded-lg shadow-md z-50">
+        {children}
+      </div>
+    </>,
+    portalElement
+  );
+};
+
+export default Modal;
