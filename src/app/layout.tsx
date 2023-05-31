bbe9cf48094b1bcc3f7ab5c6dbf3407aa5fec18b
mini <0miiii@naver.com>
2023-05-31 18:22:53 +0900

    feat: 모달을 띄우기 위한 overlays 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <div className="overlays" />
       <body className={inter.className}>
         <Header />
         {children}
