cec6e9fbb9b6148f4eba88f6808118ac9773bbd3
yeongmin <0miiii@naver.com>
2023-06-02 01:33:43 +0900

    design: 아이콘 추가 및 디자인 추가

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Image from "next/image";
 
 const SearchInput = () => {
-  return <input type="search" />;
+  return (
+    <div className="flex gap-2 bg-white rounded p-2">
+      <Image src="/icons/search.svg" alt="search" width={25} height={25} />
+      <input type="search" className="text-black p-1 focus:outline-0" />
+    </div>
+  );
 };
 
 export default SearchInput;
