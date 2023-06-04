8734beb3aa783bef0a83276535ae13e759ed2e38
mini <0miiii@naver.com>
2023-06-04 21:44:48 +0900

    refactor: props 추가

--- src/components/CategorySelect.tsx
+++ src/components/CategorySelect.tsx
@@ -1,15 +1,26 @@
+"use client";
+
 import React from "react";
 
 interface Props {
   categories: string[];
+  onSelect: (category: string) => void;
 }
 
-const CategorySelect: React.FC<Props> = ({ categories }) => {
+const CategorySelect: React.FC<Props> = ({ categories, onSelect }) => {
+  const selectChangeHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    onSelect(event.target.value);
+  };
+
   return (
-    <select className="text-black w-48 p-2 rounded">
-      <option disabled selected className="disabled:hidden">
+    <select
+      className="text-black w-full p-2 rounded bg-white border border-gray-300 focus:outline-none focus-within:ring-1 focus-within:ring-blue-500"
+      onChange={selectChangeHandler}
+    >
+      <option disabled selected value="" className="disabled:hidden">
         카테고리
       </option>
+      <option value="">전체</option>
       {categories.map((category) => (
         <option key={category} value={category}>
           {category}
