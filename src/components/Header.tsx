9d53d17c13c90fc8d7fae9eedbe8196f9014b846
mini <0miiii@naver.com>
2023-06-04 21:52:30 +0900

    refactor: 링크 객체로 정리

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,39 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+  const links = [
+    { name: "Posts", path: "/posts" },
+    { name: "About", path: "/about" },
+    { name: "Portfolios", path: "/portfolios" },
+    { name: "Contact", path: "/contact" },
+  ];
+
   return (
-    <header className="flex justify-between max-w-3xl mx-auto">
-      <h1>
-        <Link href="/">YeongMin.com</Link>
-      </h1>
-      <ul className="flex gap-2.5">
-        <li>
-          <Link href="/posts">posts</Link>
-        </li>
-        <li>
-          <Link href="/about">about</Link>
-        </li>
-        <li>
-          <Link href="/portfolios">portfolios</Link>
-        </li>
-        <li>
-          <Link href="/contact">contact</Link>
-        </li>
-      </ul>
+    <header className="w-full py-4 sticky top-0 bg-gray-800 text-white">
+      <div className="flex justify-between items-center max-w-3xl mx-auto">
+        <h1 className="text-xl font-bold">
+          <Link href="/">YeongMin.com</Link>
+        </h1>
+        <ul className="flex gap-4">
+          {links.map((link) => (
+            <li key={link.name}>
+              <Link
+                href={link.path}
+                className={`hover:underline ${
+                  pathname === link.path && "underline"
+                }`}
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </header>
   );
 };
