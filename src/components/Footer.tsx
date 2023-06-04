944460cb6b00dbf82bc892b5fd276444bd937831
mini <0miiii@naver.com>
2023-06-04 21:43:42 +0900

    feat: 스크롤에 따른 위치 변경 로직 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+const hasScroll = () => {
+  if (typeof window === "undefined") return false;
+  const element = document.documentElement;
+  const hasVerticalScrollbar = element.scrollHeight > element.clientHeight;
+  console.log("scrollHeight", element.scrollHeight);
+  console.log("clientHeight", element.clientHeight);
+  return hasVerticalScrollbar;
+};
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const [hasVerticalScroll, setHasVerticalScroll] = useState(false);
+
+  useEffect(() => {
+    setHasVerticalScroll(hasScroll());
+    console.log(hasScroll());
+  }, [hasVerticalScroll]);
+
+  return (
+    <footer
+      className={`w-full py-4 bg-gray-800 text-white text-center ${
+        !hasVerticalScroll && "fixed bottom-0"
+      }`}
+    >
+      <p className="text-gray-400 text-sm">&copy; 2023, All rights reserved.</p>
+    </footer>
+  );
 };
 
 export default Footer;
