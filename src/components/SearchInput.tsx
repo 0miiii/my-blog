015fded9cedaf79b3da4460f4233f57a1e40f223
mini <0miiii@naver.com>
2023-06-04 21:46:18 +0900

    feat: submit handler 추가

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,12 +1,32 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (keyword: string) => void;
+}
+
+const SearchInput: React.FC<Props> = ({ onSearch }) => {
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const search = formData.get("search")?.toString() || "";
+    onSearch(search);
+  };
+
   return (
-    <div className="flex gap-2 bg-white rounded p-2">
+    <form
+      className="w-full flex gap-2 bg-white p-2 border border-gray-300 rounded focus-within:ring-1 focus-within:ring-blue-500"
+      onSubmit={submitHandler}
+    >
       <Image src="/icons/search.svg" alt="search" width={25} height={25} />
-      <input type="search" className="text-black p-1 focus:outline-0" />
-    </div>
+      <input
+        name="search"
+        type="search"
+        className="w-full text-black p-1 focus:outline-none"
+      />
+    </form>
   );
 };
 
