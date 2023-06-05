ff4ef883922fdbf73ecc9ee4490fb99a3f12ac83
mini <0miiii@naver.com>
2023-06-05 20:15:38 +0900

    refactor: 더미데이터 제거 및 데이터페칭

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,21 +1,23 @@
 import PostList from "@/components/PostList";
 import CategorySelect from "@/components/CategorySelect";
 import SearchInput from "@/components/SearchInput";
+import TagList from "@/components/TagList";
 import { getAllPosts } from "@/services/post";
-
-const categories = ["a", "b", "c", "d"];
 
 const PostsPage = () => {
   const posts = getAllPosts();
+  const tags = [...new Set(posts.map((post) => post.tags).flat())];
+  const categories = [...new Set(posts.map((post) => post.category))];
+  const postCount = `${posts.length}개의 포스팅이 있습니다.`;
+
   return (
     <>
       <div className="flex justify-between mb-4 gap-5">
         <CategorySelect categories={categories} />
         <SearchInput />
       </div>
-      <div className="text-gray-500 mb-2">
-        {posts.length}개의 포스팅이 있습니다.
-      </div>
+      <TagList tags={tags} />
+      <div className="text-gray-500 mt-4 mb-2">{postCount}</div>
       <PostList posts={posts} />
     </>
   );
