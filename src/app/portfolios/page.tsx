2fe3b8625bed56aa31409eed6c090e775696389a
mini <0miiii@naver.com>
2023-06-05 20:16:34 +0900

    refactor: 더미데이터 제거 및 데이터페칭

--- src/app/portfolios/page.tsx
+++ src/app/portfolios/page.tsx
@@ -1,30 +1,15 @@
 import React from "react";
 import PortfolioList from "@/components/PortfolioList";
-
-const test = [
-  {
-    id: 1,
-    image: "test",
-    title: "test",
-    description: "test",
-    url: "test",
-  },
-  {
-    id: 2,
-    image: "test",
-    title: "test",
-    description: "test",
-    url: "test",
-  },
-];
+import { getAllPortfolios } from "@/services/portfolio";
 
 const PortfoliosPage = () => {
+  const portfolios = getAllPortfolios();
   return (
     <>
       <p className="text-gray-600 mb-4">
         지금까지 진행한 프로젝트를 담은 페이지입니다.
       </p>
-      <PortfolioList portfolios={test} />
+      <PortfolioList portfolios={portfolios} />
     </>
   );
 };
