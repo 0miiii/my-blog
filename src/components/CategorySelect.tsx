4ff203f762196dd1f156407b4da253cb5882a339
mini <0miiii@naver.com>
2023-06-08 00:04:32 +0900

    feat: 카테고리 변경시 url 변경

--- src/components/CategorySelect.tsx
+++ src/components/CategorySelect.tsx
@@ -1,21 +1,27 @@
 "use client";
 
 import React from "react";
+import { useSearchParams, useRouter } from "next/navigation";
 
 interface Props {
   categories: string[];
-  onSelect?: (category: string) => void;
 }
 
-const CategorySelect: React.FC<Props> = ({ categories, onSelect }) => {
-  // const selectChangeHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
-  //   onSelect(event.target.value);
-  // };
+const CategorySelect: React.FC<Props> = ({ categories }) => {
+  const router = useRouter();
+  const params = useSearchParams().get("category");
+
+  const selectChangeHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const select = event.target.value;
+    const url = select ? `/posts?category=${event.target.value}` : "/posts";
+    router.push(url);
+  };
 
   return (
     <select
       className="text-black w-full p-2 rounded bg-white border border-gray-300 focus:outline-none focus-within:ring-1 focus-within:ring-blue-500"
-      // onChange={selectChangeHandler}
+      onChange={selectChangeHandler}
+      value={params || ""}
     >
       <option disabled selected value="" className="disabled:hidden">
         카테고리
