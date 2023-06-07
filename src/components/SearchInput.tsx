20f294df446a6b841ab8357b255a2de2c1f79197
mini <0miiii@naver.com>
2023-06-08 00:04:04 +0900

    feat: 서비스 준비중 모달 추가

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,31 +2,38 @@
 
 import React from "react";
 import Image from "next/image";
+import Modal from "./Modal";
+import { useModal } from "@/hooks";
 
-interface Props {
-  onSearch?: (keyword: string) => void;
-}
-
-const SearchInput: React.FC<Props> = ({ onSearch }) => {
+const SearchInput = () => {
+  const { showModal, closeModalHandler, showModalHandler } = useModal();
   const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const search = formData.get("search")?.toString() || "";
-    // onSearch(search);
+
+    showModalHandler();
   };
 
   return (
-    <form
-      className="w-full flex gap-2 bg-white p-2 border border-gray-300 rounded focus-within:ring-1 focus-within:ring-blue-500"
-      onSubmit={submitHandler}
-    >
-      <Image src="/icons/search.svg" alt="search" width={25} height={25} />
-      <input
-        name="search"
-        type="search"
-        className="w-full text-black p-1 focus:outline-none"
-      />
-    </form>
+    <>
+      {showModal && (
+        <Modal onClose={closeModalHandler}>
+          <div className="p-5">서비스 준비중입니다.</div>
+        </Modal>
+      )}
+      <form
+        className="w-full flex gap-2 bg-white p-2 border border-gray-300 rounded focus-within:ring-1 focus-within:ring-blue-500"
+        onSubmit={submitHandler}
+      >
+        <Image src="/icons/search.svg" alt="search" width={25} height={25} />
+        <input
+          name="search"
+          type="search"
+          className="w-full text-black p-1 focus:outline-none"
+        />
+      </form>
+    </>
   );
 };
 
