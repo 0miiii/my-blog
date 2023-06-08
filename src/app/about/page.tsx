ddcda2a9aec8b770b2929c72f8af630ca9614f63
mini <0miiii@naver.com>
2023-06-08 21:19:59 +0900

    feat: timeline 적용

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import TimeLineList from "@/components/TimeLineList";
+
+const dummy_careers = [
+  {
+    company: "회사A",
+    position: "Front-end Developer",
+    year: "2023 - current",
+    description: "테스트 중입니다.",
+  },
+  {
+    company: "회사B",
+    position: "Front-end Developer",
+    year: "2022 - 2023",
+    description: "테스트 중입니다.",
+  },
+];
 
 const AboutPage = () => {
-  return <div>about</div>;
+  return (
+    <>
+      <section className="mb-8">
+        <h1 className="text-2xl font-bold mb-2">Career</h1>
+        <TimeLineList timeline={dummy_careers} />
+      </section>
+      <section>
+        <h1 className="text-2xl font-bold mb-2">Activity</h1>
+        <TimeLineList timeline={dummy_careers} />
+      </section>
+    </>
+  );
 };
 
 export default AboutPage;
