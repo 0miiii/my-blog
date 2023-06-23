503e475de84b81b0629adddbbda36f98c6bc9d60
mini <0miiii@naver.com>
2023-06-24 00:39:45 +0900

    feat: 스크롤막기, esc키 닫기 기능 추가

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,26 +1,57 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 
 interface IProps {
   children: React.ReactNode;
   onClose: () => void;
+  isOpen: boolean;
 }
 
-const Modal: React.FC<IProps> = ({ children, onClose }) => {
-  if (typeof window === "undefined") return null;
+const Modal: React.FC<IProps> = ({ children, isOpen, onClose }) => {
+  const overlayRef = useRef<HTMLDivElement>(null);
 
-  const portalElement = document.getElementById("overlays") as
-    | Element
-    | DocumentFragment;
+  const overlayClickHandler = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === overlayRef.current) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    const escKeyDownHandler = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+      document.addEventListener("keydown", escKeyDownHandler);
+    } else {
+      document.body.style.overflow = "auto";
+      document.removeEventListener("keydown", escKeyDownHandler);
+    }
+
+    return () => {
+      document.removeEventListener("keydown", escKeyDownHandler);
+    };
+  }, [isOpen, onClose]);
+
+  if (typeof window === "undefined" || !isOpen) return null;
+
+  const portalElement = document.getElementById("overlays") as Element;
 
   return ReactDOM.createPortal(
     <>
       <div
         className="fixed top-0 left-0 w-full h-screen z-40 bg-black bg-opacity-75"
-        onClick={onClose}
-      />
-      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 mx-auto bg-white rounded-lg shadow-md z-50">
-        {children}
+        ref={overlayRef}
+        onClick={overlayClickHandler}
+      >
+        <div className="fixed z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 bg-white rounded-lg shadow-md">
+          {children}
+        </div>
       </div>
     </>,
     portalElement
