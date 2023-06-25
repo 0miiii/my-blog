da074ffd3bef4ed1d1f5a02396e74e686e598061
mini <0miiii@naver.com>
2023-06-25 16:49:08 +0900

    refactor: 기존 코드를 컴포넌트로 변경

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,31 +1,11 @@
-import PostList from "@/components/PostList";
-import CategorySelect from "@/components/CategorySelect";
-import SearchInput from "@/components/SearchInput";
-import TagList from "@/components/TagList";
+import React from "react";
+import FilteredPost from "@/components/FilteredPost";
 import { getAllPosts } from "@/services/post";
 
-const PostsPage = (props: any) => {
+const PostsPage = () => {
   const posts = getAllPosts();
-  const tags = [...new Set(posts.map((post) => post.tags).flat())];
-  const categories = [...new Set(posts.map((post) => post.category))];
 
-  const params = props.searchParams.category;
-  const filteredPosts = params
-    ? posts.filter((post) => post.category === params)
-    : posts;
-  const postCount = `${filteredPosts.length}개의 포스팅이 있습니다.`;
-
-  return (
-    <>
-      <div className="flex justify-between mb-4 gap-5">
-        <CategorySelect categories={categories} />
-        <SearchInput />
-      </div>
-      <TagList tags={tags} />
-      <div className="text-gray-500 mt-4 mb-2">{postCount}</div>
-      <PostList posts={filteredPosts} />
-    </>
-  );
+  return <FilteredPost posts={posts} />;
 };
 
 export default PostsPage;
