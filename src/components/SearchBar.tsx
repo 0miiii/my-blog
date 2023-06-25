843cbc1207d08204048a3d8b5509cfd7f55d9e28
mini <0miiii@naver.com>
2023-06-25 17:34:29 +0900

    refactor: ref를 통해 검색된 값 상태끌어올리기

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,39 +1,33 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
-import Modal from "./Modal";
-import { useModal } from "@/hooks";
 
-const SearchBar = () => {
-  const { isOpen, closeModalHandler, showModalHandler } = useModal();
+interface IProps {
+  onSearch: (search: string) => void;
+}
+
+const SearchBar: React.FC<IProps> = ({ onSearch }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const search = formData.get("search")?.toString() || "";
-
-    showModalHandler();
+    inputRef.current && onSearch(inputRef.current.value);
   };
 
   return (
-    <>
-      <Modal isOpen={isOpen} onClose={closeModalHandler}>
-        <div className="p-5">서비스 준비중입니다.</div>
-      </Modal>
-
-      <form
-        className="w-full flex gap-2 bg-white p-2 border border-gray-300 rounded focus-within:ring-1 focus-within:ring-blue-500"
-        onSubmit={submitHandler}
-      >
-        <Image src="/icons/search.svg" alt="search" width={25} height={25} />
-        <input
-          name="search"
-          type="search"
-          className="w-full text-black p-1 focus:outline-none"
-        />
-      </form>
-    </>
+    <form
+      className="w-full flex gap-2 bg-white p-2 border border-gray-300 rounded focus-within:ring-1 focus-within:ring-blue-500"
+      onSubmit={submitHandler}
+    >
+      <Image src="/icons/search.svg" alt="search" width={25} height={25} />
+      <input
+        name="search"
+        type="search"
+        className="w-full text-black p-1 focus:outline-none"
+        ref={inputRef}
+      />
+    </form>
   );
 };
 
