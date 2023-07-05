95a73489be2da867e314141c86854a28e5a18141
mini <0miiii@naver.com>
2023-07-05 14:37:18 +0900

    feat: onClick 기능과 activity 상태추가
    activity 상태를 추가하여 상태에 따라 스타일이 변경됩니다.
    
    클릭이벤트가 발생되면 onClick 함수를 호출하고 activity 상태를 업데이트합니다.

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
-  children: React.ReactNode;
+  children: string;
+  onClick: (tag: string) => void;
 }
 
-const Tag: React.FC<Props> = ({ children }) => {
+const Tag: React.FC<Props> = ({ children, onClick }) => {
+  const [activity, setActivity] = useState(false);
+
+  const clickHandler = () => {
+    onClick(children);
+    setActivity(!activity);
+  };
+
   return (
-    <span className="inline-block px-2 py-1 bg-gray-300 text-gray-800 rounded-md text-sm cursor-pointer hover:bg-blue-500 hover:text-white">
+    <span
+      className={`inline-block px-2 py-1 rounded-md text-sm cursor-pointer ${
+        activity
+          ? "bg-blue-500 text-white"
+          : "bg-gray-300 text-gray-800 hover:bg-blue-500 hover:text-white"
+      }`}
+      onClick={clickHandler}
+    >
       {children}
     </span>
   );
